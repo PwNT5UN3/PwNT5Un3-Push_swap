55e0da5e1c7cb5a7b7150a1cb779b7ed60cc0986
Maximilian Felix Welsch <mawelsch@student.42heilbronn.de>
2025-11-10 23:50:35 +0100

    retrying

--- simple_sort.c
+++ simple_sort.c
@@ -6,7 +6,7 @@
 /*   By: mawelsch <mawelsch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 17:55:24 by mawelsch          #+#    #+#             */
-/*   Updated: 2025/11/10 23:21:19 by mawelsch         ###   ########.fr       */
+/*   Updated: 2025/11/10 23:41:50 by mawelsch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,19 @@ void	bubblesort(t_stk *stk, t_stat *stats, t_flag *flags)
 
 	while (ft_get_disorder(stk->vals, stk->len))
 	{
-		left = stk->len - 2;
+		left = stk->len - 1;
 		index = 0;
-		while (!left)
+		while (index < left)
 		{
-			while (index < left)
+			if (stk->vals[index] > stk->vals[index + 1])
 			{
-				if (stk->vals[index] > stk->vals[index + 1])
-				{
-					left -= index;
-					while (index--)
-						r(stk, stats, flags);
-					s(stk, stats, flags);
-				}
-				index++;
+				left -= index;
+				while (index--)
+					r(stk, stats, flags);
+				s(stk, stats, flags);
 			}
+			index++;
+			r(stk, stats, flags);
 		}
 	}
 }
