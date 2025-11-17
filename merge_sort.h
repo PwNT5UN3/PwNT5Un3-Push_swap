d2e960e207c2d393efbd7becd8deb36c0115717c
Maximilian Felix Welsch <mawelsch@student.42heilbronn.de>
2025-11-17 03:34:07 +0100

    added list_chaanging based optimiasation

--- merge_sort.h
+++ merge_sort.h
@@ -6,7 +6,7 @@
 /*   By: mawelsch <mawelsch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 18:33:12 by mawelsch          #+#    #+#             */
-/*   Updated: 2025/11/16 18:34:16 by mawelsch         ###   ########.fr       */
+/*   Updated: 2025/11/17 02:19:29 by mawelsch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,20 @@ typedef struct s_lists
 	size_t	l2l;
 	int		must_rotate;
 }	t_lists;
+
+void	small_merger_list_1(t_stk *a, t_stk *b, t_lists *lsts, int *active_lst);
+void	big_merger_list_1(t_stk *a, t_stk *b, t_lists *lsts, int *active_lst);
+void	small_merger_list_2(t_stk *a, t_stk *b, t_lists *lsts, int *active_lst);
+void	big_merger_list_2(t_stk *a, t_stk *b, t_lists *lsts, int *active_lst);
+
+size_t	get_first_list_size_a(t_stk *stk);
+size_t	get_first_list_size_b(t_stk *stk);
+size_t	get_last_list_size_a(t_stk *stk, size_t l1l);
+size_t	get_last_list_size_b(t_stk *stk, size_t l1l);
+
+void	slide_to_top_bottom_b(t_stk *a, t_stk *b, size_t *list);
+void	slide_to_top_bottom_a(t_stk *a, t_stk *b, size_t *list);
+
+void	switch_sublist(t_lists *lsts, t_stk *stk, int *active_list);
 
 #endif //MERGE_SORT_H
--- merge_utils_a.c
+++ merge_utils_a.c
@@ -6,61 +6,68 @@
 /*   By: mawelsch <mawelsch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 18:23:06 by mawelsch          #+#    #+#             */
-/*   Updated: 2025/11/17 01:05:27 by mawelsch         ###   ########.fr       */
+/*   Updated: 2025/11/17 03:21:37 by mawelsch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "merge_sort.h"
 
-void	slide_to_top_bottom_a(t_stk *a, t_stk *b, t_lists *lsts)
+void	slide_to_top_bottom_a(t_stk *a, t_stk *b, size_t *list)
 {
-	while (lsts->l1l)
+	while (*list)
 	{
 		if (a->vals[0] > b->vals[0] && b->len > 0)
 		{
 			p(b, a);
-			lsts->l1l -= 1;
+			*list -= 1;
 			continue ;
 		}
 		if (a->vals[0] < b->vals[b->len - 1] && b->len > 0)
 		{
 			p(b, a);
 			r(b);
-			lsts->l1l -= 1;
+			*list -= 1;
 			continue ;
 		}
 		break ;
 	}
 }
 
-void	small_merger(t_stk *a, t_stk *b, t_lists *lsts)
+void	small_merger_list_1(t_stk *a, t_stk *b, t_lists *lsts, int *active_lst)
 {
-	int	rotator;
+	if (a->vals[0] < b->vals[b->len - 1] && b->len > 0)
+	{
+		p(b, a);
+		r(b);
+		lsts->l1l -= 1;
+	}
+	else if (a->vals[0] < a->vals[lsts->l1l])
+	{
+		p(b, a);
+		lsts->l1l -= 1;
+	}
+	else
+	{
+		switch_sublist(lsts, a, active_lst);
+	}
+}
 
-	slide_to_top_bottom_a(a, b, lsts);
-	while (lsts->l1l && lsts->l2l)
-	{		
-		if (a->vals[0] < b->vals[b->len - 1] && b->len > 0)
-		{
-			p(b, a);
-			r(b);
-			lsts->l1l -= 1;
-		}
-		if (a->vals[0] < a->vals[lsts->l1l])
-		{
-			p(b, a);
-			lsts->l1l -= 1;
-		}
-		else
-		{
-			rotator = 0;
-			while (rotator++ < lsts->l1l - 1)
-				r(a);
-			s(a);
-			p(b, a);
-			while (--rotator > 0)
-				rr(a);
-			lsts->l2l -= 1;
-		}
+void	small_merger_list_2(t_stk *a, t_stk *b, t_lists *lsts, int *active_lst)
+{
+	if (a->vals[0] < b->vals[b->len - 1] && b->len > 0)
+	{
+		p(b, a);
+		r(b);
+		lsts->l2l -= 1;
+	}
+	else if (a->vals[0] < a->vals[a->len - lsts->l1l])
+	{
+		p(b, a);
+		lsts->l2l -= 1;
+	}
+	else
+	{
+		switch_sublist(lsts, a, active_lst);
 	}
 }
